9939583ad9f3eedfde62642781396bbac7cde12c
lyxwll <jyu4228716@163.com>
2015-08-05 21:05:43 +0800

    效果图实例

---
+++ src/com/zftlive/android/entity/Config.java
@@ -0,0 +1,12 @@
+package com.zftlive.android.entity;
+
+import com.zftlive.android.base.BaseEntity;
+
+public class Config extends BaseEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
